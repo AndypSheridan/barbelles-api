a4514b0f23c42db9728edb8b55d0f89442f5b043
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-08 16:46:24 +0000

    Import os in env.py, add cloudinary url in settings.py

--- barbelles_api/settings.py
+++ barbelles_api/settings.py
@@ -11,6 +11,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
+CLOUDINARY_STORAGE = {
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
+}
+
+MEDIA_URL = '/media/'
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
+if os.path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
