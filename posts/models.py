0fe9a59de0ae7ddac3e57cd30dbe98270bd9a2fb
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-14 17:31:07 +0000

    Create posts model and migrate changes

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Post model, related to User instance
+    Allows users to add posts with images
+    Default image allows reference of image.url
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    story = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_eirfz0', blank=True
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id}{self.title}'
