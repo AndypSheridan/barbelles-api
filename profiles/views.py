69bd9f077ae847ce1e0c5f8cb4b98ebbf1ff1025
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-14 16:02:29 +0000

    Import Http404 in views.py

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from django.http import Http404
 
 
 class ProfileList(APIView):
