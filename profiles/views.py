a6ba1c40d70ed2382609d9ce764eb79b057a0913
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-14 15:14:00 +0000

    Create ProfileList class in views.py

--- profiles/views.py
+++ profiles/views.py
@@ -3,4 +3,7 @@ from rest_framework.response import Response
 from .models import Profile
 
 
-
+class ProfileList(APIView):
+    def get(self, request):
+        profiles = Profile.objects.all()
+        return Response(profiles)
