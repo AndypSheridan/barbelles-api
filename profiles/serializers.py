35d3d274962840b8644e4740210f6e9568ce800e
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-14 17:00:34 +0000

    Add get_is_owner method in serializers.py

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,10 +3,16 @@ from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'name', 'bio', 'image', 'created_at', 'updated_at'
+            'id', 'owner', 'name', 'bio', 'image', 'created_at', 'updated_at', 'is_owner',
         ]
