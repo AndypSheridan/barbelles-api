69efbdc036f7f47b8f97f193d31bb8390798d084
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-14 16:54:16 +0000

    Add permissions to get object method in views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from barbelles_api import IsOwnerOrReadOnly
+from barbelles_api.permissions import IsOwnerOrReadOnly
 from .serializers import ProfileSerializer
 from rest_framework.views import APIView
 from rest_framework import status
@@ -22,6 +22,7 @@ class ProfileDetail(APIView):
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
