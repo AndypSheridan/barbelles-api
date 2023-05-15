d8a8224aec11b2de3eb08096d0b03f6ad9748619
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-15 17:07:23 +0000

    Add tutorial list and detail views in views.py

--- tutorials/views.py
+++ tutorials/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import permissions, generics, filters
+from barbelles_api.permissions import IsOwnerOrReadOnly
+from .serializers import TutorialSerializer
+from django.db.models import Count
+from django.http import Http404
+from .models import Tutorial
 
-# Create your views here.
+
+class TutorialList(generics.ListCreateAPIView):
+
+    serializer_class = TutorialSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    queryset = Tutorial.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owener=self.request.user)
+
+
+class TutorialDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    serializer_class = TutorialSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Tutorial.objects.all()
