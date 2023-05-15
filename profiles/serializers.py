059a6f5296e054141d92bf81bf03cf8d51d84c39
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-15 13:36:38 +0000

    Add filters to profile list and profile detail

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -8,6 +8,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -27,5 +30,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'name', 'bio', 'image',
             'created_at', 'updated_at', 'is_owner', 
-            'following_id',
+            'following_id', 'posts_count', 'followers_count',
+            'following_count'
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
-from rest_framework import generics, permissions, filters
 from barbelles_api.permissions import IsOwnerOrReadOnly
+from rest_framework import generics, filters
 from .serializers import ProfileSerializer
 from django.db.models import Count
 from django.http import Http404
@@ -8,13 +8,30 @@ from .models import Profile
 
 class ProfileList(generics.ListAPIView):
 
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post',distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
 
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post',distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
