77f885f4d26d3f1ba4cdb00cc6fd555fbd4cc4e9
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-15 07:57:08 +0000

    Add image filter field to Post model in models.py

--- posts/models.py
+++ posts/models.py
@@ -18,13 +18,15 @@ class Post(models.Model):
     ('toaster', 'Toaster'), ('valencia', 'Valencia'),
     ('walden', 'Walden'), ('xpro2', 'X-pro II')
     ]
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     story = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_post_eirfz0', blank=True
+        )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default=normal
         )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
