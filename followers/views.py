81c2dad2356b37428945560810798907501e4410
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-15 11:55:58 +0000

    Add urls.py and file paths

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from barbelles_api.permissions import IsOwnerOrReadOnly
+from rest_framework import generics, permissions
+from .serializers import FollowerSerializer
+from .models import Follower
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FolowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieves or destroys a 'follow'
+    Update view not necessary
+    """
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
