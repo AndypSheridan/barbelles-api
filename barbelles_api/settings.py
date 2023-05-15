380976454eb6f212a11512b0ae818d48eccf6305
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-15 10:41:27 +0000

    Start likes app and add to settings.py

--- barbelles_api/settings.py
+++ barbelles_api/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'posts',
     'workouts',
     'comments',
+    'likes',
 ]
 
 MIDDLEWARE = [
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,6 @@
 from rest_framework import status, generics, permissions
 from barbelles_api.permissions import IsOwnerOrReadOnly
-from rest_framework.response import Response
 from .serializers import ProfileSerializer
-from rest_framework.views import APIView
 from django.http import Http404
 from .models import Profile
 
@@ -13,41 +11,9 @@ class ProfileList(generics.ListAPIView):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
 
-    # def get(self, request):
-    #     profiles = Profile.objects.all()
-    #     serializer = ProfileSerializer(
-    #         profiles, many=True, context={'request': request}
-    #         )
-    #     return Response(serializer.data)
-
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
 
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.all()
-
-    # def get_object(self, pk):
-    #     try:
-    #         profile = Profile.objects.get(pk=pk)
-    #         self.check_object_permissions(self.request, profile)
-    #         return profile
-    #     except Profile.DoesNotExist:
-    #         raise Http404
-
-    # def get(self, request, pk):
-    #     profile = self.get_object(pk)
-    #     serializer = ProfileSerializer(
-    #         profile, context={'request': request}
-    #         )
-    #     return Response(serializer.data)
-
-    # def put(self, request, pk):
-    #     profile = self.get_object(pk)
-    #     serializer = ProfileSerializer(
-    #         profile, data=request.data, context={'request': request}
-    #         )
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
