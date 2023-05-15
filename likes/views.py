0853204bf170c301b67fd35db9e8007f28bf33b4
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-15 11:04:35 +0000

    Create Like list view in likes.view.py

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from barbelles_api.permissions import IsOwnerOrReadOnly
+from rest_framework import generics, permissions
+from .serializers import LikeSerializer
+from .models import Like
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    Lists all likes. If authenticated, User can create a like.
+    Perform_create links like to current logged in User
+    """
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
