3fc297ce9a7d48fc426e162333c2b30e86246d22
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-15 10:06:00 +0000

    Add Comment detail view and test in browser

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from .serializers import CommentSerializer, CommentDetailSerializer
+from barbelles_api.permissions import IsOwnerOrReadOnly
+from rest_framework import generics, permissions
+from .models import Comment
 
-# Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
