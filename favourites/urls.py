d1928e17212ae00d6f943a8306a12af4dd8f88e0
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-16 20:21:02 +0000

    Add favourites url patterns in urls.py

---
+++ favourites/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from favourites import views
+
+
+urlpatterns = [
+    path('favourites/', views.FavouriteList.as_view()),
+    path('favourites/<int:pk>/', views.FavouriteDetail.as_view()),
+]
