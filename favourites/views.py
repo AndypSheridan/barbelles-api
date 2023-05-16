e875b27f296b0ad268827cf5cf1f3a115b7b8f4e
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-16 20:19:01 +0000

    Create FavouriteLIst,Detail in views.py

--- favourites/views.py
+++ favourites/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from barbelles_api.permissions import IsOwnerOrReadOnly
+from rest_framework import generics, permissions
+from .serializers import FavouriteSerializer
+from .models import Favourite
 
-# Create your views here.
+
+class FavouriteList(generics.ListCreateAPIView):
+    """
+    Lists all favourites. If authenticated, User can create a favourite.
+    Perform_create links favourite to current logged in User
+    """
+    serializer_class = FavouriteSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Favourite.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FavouriteDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieves or destroys a favourite
+    Update view not necessary
+    """
+    serializer_class = FavouriteSerializer()
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Favourite.objects.all()
+
