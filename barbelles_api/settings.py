127941e87c619075fe77a46ca312a4e7f7a15e9a
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-18 12:12:02 +0000

    Import re in settings.py

--- barbelles_api/settings.py
+++ barbelles_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import re
 import dj_database_url
 
 if os.path.exists('env.py'):
@@ -40,7 +41,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'barbelles-api.herokuapp.com']
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+    ]
 
 
 # Application definition
