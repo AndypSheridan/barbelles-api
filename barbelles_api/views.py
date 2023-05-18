f6d3d20fc125c5a55c81f3711520a2b812a670c3
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-18 12:03:50 +0000

    Add fix for dj-rest-auth logout bug

--- barbelles_api/views.py
+++ barbelles_api/views.py
@@ -10,3 +10,27 @@ def root_route(request):
     return Response({
         'Message': 'Welcome to the BarBelles API, developed with Django Rest Framework'
         })
+
+# Fix for dj-rest-auth logout bug
+@api_view(['POST'])
+def logout_route(request):
+    response = Response()
+    response.set_cookie(
+        key = JWT_AUTH_COOKIE,
+        value = '',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
