307eab80bd11bebb7f1d71292d3d6d5fb4743dec
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-30 17:07:21 +0000

    Change video field in tutorials.models.py

--- tutorials/models.py
+++ tutorials/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from  embed_video.fields  import  EmbedVideoField
 
 
 class Tutorial(models.Model):
@@ -12,7 +11,7 @@ class Tutorial(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     summary = models.TextField(blank=True)
-    video = EmbedVideoField()
+    video = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
