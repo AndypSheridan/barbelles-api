410924a20305e1cdf9db4c4991287f28055e4759
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-30 17:15:57 +0000

    Add extra fields in profiles.models.py

--- profiles/models.py
+++ profiles/models.py
@@ -10,6 +10,8 @@ class Profile(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../default_profile_sigwcy'
     )
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
