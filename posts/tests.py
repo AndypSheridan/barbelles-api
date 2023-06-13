e3f01323d9baf6b2638bbaf45f838985fe5c80be
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-13 12:04:37 +0000

    Create PostListViewTests in posts.tests.py

--- posts/tests.py
+++ posts/tests.py
@@ -1,4 +1,9 @@
 from django.contrib.auth.models import User
 from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create(username='admin',password='testpasswordadmin')
