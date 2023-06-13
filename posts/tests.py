fcb904ed5cfe0aa0f87303e29a8c9c36c48ffff5
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-13 12:09:53 +0000

    Remove tests in tests.py

--- posts/tests.py
+++ posts/tests.py
@@ -1,15 +1,1 @@
-from django.contrib.auth.models import User
-from .models import Post
-from rest_framework import status
-from rest_framework.test import APITestCase
 
-
-class PostListViewTests(APITestCase):
-    def setUp(self):
-        User.objects.create(username='admin',password='testpasswordadmin')
-
-    def test_can_list_posts(self):
-        admin = User.objects.get(username='admin')
-        Post.objects.create(owner=admin, title='Tennis team')
-        response = self.client.get('/posts/')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
