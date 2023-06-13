9d3f19a39a081dc5e851984897b79e48ddb0a0cd
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-13 12:08:25 +0000

    Create user can list posts test in posts.tests.py

--- posts/tests.py
+++ posts/tests.py
@@ -7,3 +7,9 @@ from rest_framework.test import APITestCase
 class PostListViewTests(APITestCase):
     def setUp(self):
         User.objects.create(username='admin',password='testpasswordadmin')
+
+    def test_can_list_posts(self):
+        admin = User.objects.get(username='admin')
+        Post.objects.create(owner=admin, title='Tennis team')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
