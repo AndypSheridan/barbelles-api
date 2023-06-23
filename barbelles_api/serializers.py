c83eeb99dcf5b1351a0dfba41a08552bf966ec50
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-23 18:48:12 +0000

    Add docstrings in main app serializers.py file.

--- barbelles_api/serializers.py
+++ barbelles_api/serializers.py
@@ -3,13 +3,17 @@ from rest_framework import serializers
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
-
+    """
+    Generate user information to display when user logs in.
+    """
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
     profile_is_staff = serializers.ReadOnlyField(source='profile.is_staff')
 
     class Meta(UserDetailsSerializer.Meta):
-
+        """
+        Fields to display.
+        """
         fields = UserDetailsSerializer.Meta.fields + (
             'profile_id', 'profile_image', 'profile_is_staff',
         )
