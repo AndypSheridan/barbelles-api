dd587791cf76961aaaf1c7e4485efb1cefcdd2b8
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-23 17:54:28 +0000

    Add dotrings in likes.serializers.py

--- likes/serializers.py
+++ likes/serializers.py
@@ -4,7 +4,9 @@ from .models import Like
 
 
 class LikeSerializer(serializers.ModelSerializer):
-
+    """
+    Serializer for Like Model.
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
@@ -14,6 +16,10 @@ class LikeSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
+        """
+        Displays an error message if the user
+        tries to like a post more than once.
+        """
         try:
             return super().create(validated_data)
         except IntegrityError:
